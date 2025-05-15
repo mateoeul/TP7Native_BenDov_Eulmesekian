a0cdb37e1f2a12c0ba14052cc5df0ae5b216ccb7
Android Studio <devandroid@ort>
2025-05-15 12:10:03 -0300

    estilo terminado

--- tp7/App.js
+++ tp7/App.js
@@ -33,35 +33,47 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-            <View style={{flex: 1, backgroundColor: 'purple', width: '100%'}}>
-        <Text>Login App</Text>
+      
+      <View style={{marginBottom: 25}}>  
+        <Text style={{color: 'white', fontSize: 15, marginTop: 8}}>Login App</Text>
       </View>
 
-      <StatusBar />
+      <View style={styles.componentsContainer}>
+        <StatusBar style='dark'/>
 
-      <Image source={require("./assets/splash.png")} style={{width:'80%', height: '40%', marginBottom:20}}/>
+        <Image source={require("./assets/splash.png")} style={{width:'80%', height: '40%', marginBottom:20}}/>
 
-      <InputText placeholder="Email"> </InputText>
-      <InputText placeholder="Contraseña"> </InputText>
+        <InputText placeholder="Email"> </InputText>
+        <InputText placeholder="Contraseña"> </InputText>
 
-      <Button titulo="WICNUDEL"> </Button>
-      
-      <Text style={styles.texto}>¿Olvidaste la contraseña?</Text>
-      <Text style={styles.texto}>Crear cuenta</Text>
+        <Button titulo="WICNUDEL"> </Button>
+
+        <Text style={styles.texto}>¿Olvidaste la contraseña?</Text>
+        <Text style={styles.texto}>Crear cuenta</Text>
+      </View>
+
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#c527d6',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   texto: {
     marginBottom: 10,
     fontWeight: 'bold'
   }, 
+  componentsContainer:{
+    backgroundColor: 'white', 
+    flex:3, 
+    width:'100%', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    height:'100%'
+  },
   
 });
